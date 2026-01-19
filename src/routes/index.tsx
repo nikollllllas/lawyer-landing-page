9d63d64e92b279d2eb29649d70ac25316b2189f9
nikollllllas <nikollasohta2014@gmail.com>
2026-01-19 20:43:32 -0300

    fix: remove Link import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import { Mail, MapPin, MoveDownIcon, Phone, Scale, Sparkles } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 
